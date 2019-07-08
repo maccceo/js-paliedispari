bb4a693b609b5f25ed3f1aa0e837c80220dcd5cf
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-08 13:34:16 +0200

    Parola palindroma - esercizio completato

--- palindromo/main.js
+++ palindromo/main.js
@@ -1,1 +1,21 @@
 // funzione che capisce se la parola inserita è palindroma
+
+var input = prompt("Inserisci la parola");
+var output = Palindromo(input);
+alert(output);
+
+
+function Palindromo(parola) {
+	//init indice ultima lettera parola
+	var indiceContrario = parola.length - 1;
+
+	for (var i = 0; i < parola.length; i++) {
+		if (parola[i] != parola[indiceContrario]) {
+			return "La parola non è palindroma";
+		} else {
+			indiceContrario--;
+		}
+	}
+	// return solo nel caso in cui non siano emerse coppie diverse
+	return "La parola è palindroma";
+}
