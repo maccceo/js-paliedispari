082dbb4e0973ee119e6a0f3ff6d7ceb4e22ce278
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-08 15:39:25 +0200

    Pari o dispari - definizione step procedurali

--- paridispari/main.js
+++ paridispari/main.js
@@ -1,3 +1,13 @@
 // pari o dispari contro il computer:
 // prompt utente "pari o dispari" e numero da 1 a 5
 // genera numero da 1 a 5 per il computer e dichiara chi ha vinto
+
+//acquisisco input utente
+var choice = prompt("Pari o dispari?","Pari");
+var playerNumeber = prompt("Inserisci un numero da 1 a 5:");
+
+//genero numero computer
+var comNumber = RandomGenerator();
+
+//trovo vincitore e lo stampo
+Winner(choice, playerNumeber, comNumber);
