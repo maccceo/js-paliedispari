503de02d114d3683e5b998e99851ffd1ea0d5899
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-08 15:45:51 +0200

    Pari e dispari - input acquisiti, numero computer generato

--- paridispari/main.js
+++ paridispari/main.js
@@ -2,12 +2,27 @@
 // prompt utente "pari o dispari" e numero da 1 a 5
 // genera numero da 1 a 5 per il computer e dichiara chi ha vinto
 
+var choice, playerNumeber, comNumber;
+
+
 //acquisisco input utente
-var choice = prompt("Pari o dispari?","Pari");
-var playerNumeber = prompt("Inserisci un numero da 1 a 5:");
+choice = prompt("Pari o dispari?","Pari");
+choice = choice.toLowerCase();
+playerNumeber = parseInt(prompt("Inserisci un numero da 1 a 5:"));
+console.log(choice + ' ' + playerNumeber);
 
 //genero numero computer
-var comNumber = RandomGenerator();
+comNumber = RandomGenerator();
 
 //trovo vincitore e lo stampo
 Winner(choice, playerNumeber, comNumber);
+
+
+
+
+// # # FUNZIONI # #
+function RandomGenerator () {
+	var number = Math.floor(Math.random() * 5 + 1);
+	console.log(number);
+	return number;
+}
